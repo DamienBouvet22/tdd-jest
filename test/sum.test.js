416e811df9db509bb292af62bf64a2f6cbc059f2
DamienBouvet22 <damien.bouvet@le-campus-numerique.fr>
2020-08-26 14:44:12 +0200

    Add first tests

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,17 @@
 // Add the unit tests of the sum function here
+
+const sum = require('../src/sum');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test ('1 argument of sum is a string', () => {
+    expect(sum('1',2)).toEqual(expect.stringContaining('error'));
+
+});
+
+test ('both arguments of sum are strings', () => {
+    expect(sum('1','toto')).toEqual(expect.stringContaining('error'));
+
+});
---
+++ test/sum2.test.js
@@ -0,0 +1,8 @@
+// Add the unit tests of the sum function here
+
+const sum2 = require('../src/sum2');
+
+
+test('toto', () => {
+    expect(sum2(1, 2)).toBe(3);
+});
